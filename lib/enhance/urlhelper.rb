e2925e83e71395fd82999add0b8913f867ea987c
Guillaume Malette <gmalette@mentel.com>
2011-08-29 15:37:08 -0400

    Fixes enhance!'s query string part.

--- lib/enhance/urlhelper.rb
+++ lib/enhance/urlhelper.rb
@@ -1,6 +1,6 @@
 module ActionView::Helpers::UrlHelper
   def enhance url, geometry
-    matches = url.match /(?<url>.*)\??(?<get>.*)?/
+    matches = url.match /(?<url>[^?])\??(?<get>.*)?/
     [[matches['url'], geometry].compact.join("/"), matches['get']].compact.reject(&:empty?).join("?")
   end
   alias_method :enhance!, :enhance
